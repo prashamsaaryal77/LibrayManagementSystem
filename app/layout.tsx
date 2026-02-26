59b33bdc10e29315395d4488811f4bf975a9e9d5
Prashamsa aryal <prashamsaaryal77@gmail.com>
2026-02-26 09:09:44 +0545

    Return modifications and book stock to be added:

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,23 +10,23 @@ export const metadata: Metadata = {
   title: 'LMS',
   description: 'Library Management System',
   generator: '',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
+  // icons: {
+  //   icon: [
+  //     {
+  //       url: '/icon-light-32x32.png',
+  //       media: '(prefers-color-scheme: light)',
+  //     },
+  //     {
+  //       url: '/icon-dark-32x32.png',
+  //       media: '(prefers-color-scheme: dark)',
+  //     },
+  //     {
+  //       url: '',
+  //       type: 'image/svg+xml',
+  //     },
+  //   ],
+  //   apple: '/apple-icon.png',
+  // },
 }
 
 export default function RootLayout({
